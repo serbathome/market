8b94034a4a58d34d359911858792954679d92e67
Usverrr <diastorero@gmail.com>
2022-07-04 10:37:45 +0300

    gitcommit

--- web/urls.py
+++ web/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
@@ -10,4 +10,7 @@ urlpatterns = [
     path('profile/', views.profile, name='profile'),
     path('api/get_categories', views.get_categories, name='get_categories'),
     path('api/get_products', views.get_products, name='get_products'),
+    re_path(r'^$', views.cart_detail, name='cart_detail'),
+    re_path(r'add/(?P<product_id>[-\w]+)/$', views.cart_add, name='cart_add'),
+    re_path(r'remove/(?P<product_id>[-\w]+)/$', views.cart_remove, name='cart_remove'),
 ]
--- web/views.py
+++ web/views.py
@@ -1,11 +1,15 @@
 from django.http import Http404, HttpResponse, HttpResponseRedirect, JsonResponse
-from django.shortcuts import render
+from django.shortcuts import render,redirect, get_object_or_404
 from django.contrib.auth import logout as do_logout
 from django.contrib.auth import login as do_login
 from django.contrib.auth import authenticate
 from .models import Profile, ProductCategory, Product
 from django.contrib.auth.models import User
 from django.core import serializers
+from django.conf import settings
+from decimal import Decimal
+from .forms import CartAddProductForm
+from django.views.decorators.http import require_POST
 
 # Create your views here
 
@@ -130,3 +134,95 @@ def get_products(request):
             'Price': product.Price
         })
     return JsonResponse(products, safe=False)
+
+#СОздание класса Корзина
+class Cart(object):
+
+    def __init__(self, request):
+        #Инициализируем корзину
+        self.session = request.session
+        cart = self.session.get(settings.CART_SESSION_ID)
+        if not cart:
+            # save an empty cart in the session
+            cart = self.session[settings.CART_SESSION_ID] = {}
+        self.cart = cart
+
+    def add(self, product, quantity=1, update_quantity=False):
+        #Добавить продукт в корзину или обновить его количество.
+        product_id = str(product.id)
+        if product_id not in self.cart:
+            self.cart[product_id] = {'quantity': 0,
+                                    'price': str(product.Price)}
+        if update_quantity:
+            self.cart[product_id]['quantity'] = quantity
+        else:
+            self.cart[product_id]['quantity'] += quantity
+        self.save()
+
+    def save(self):
+        # Обновление сессии cart
+        self.session[settings.CART_SESSION_ID] = self.cart
+        # Отметить сеанс как "измененный", чтобы убедиться, что он сохранен
+        self.session.modified = True    
+
+    def remove(self, product):
+        #Удаление товара из корзины.
+        product_id = str(product.id)
+        if product_id in self.cart:
+            del self.cart[product_id]
+            self.save()
+
+    def __iter__(self):
+        #Перебор элементов в корзине и получение продуктов из базы данных.
+        product_ids = self.cart.keys()
+        # получение объектов product и добавление их в корзину
+        products = Product.objects.filter(id__in=product_ids)
+        for product in products:
+            self.cart[str(product.id)]['product'] = product
+
+        for item in self.cart.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['quantity']
+            yield item
+
+    def __len__(self):
+        #Подсчет всех товаров в корзине.
+        return sum(item['quantity'] for item in self.cart.values())
+
+    def get_total_price(self):
+        #Подсчет стоимости товаров в корзине.
+        return sum(Decimal(item['price']) * item['quantity'] for item in
+                self.cart.values())
+
+    def clear(self):
+        # удаление корзины из сессии
+        del self.session[settings.CART_SESSION_ID]
+        self.session.modified = True
+
+
+
+
+
+@require_POST
+def cart_add(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    print('CART_add')
+    print(product)
+    form = CartAddProductForm(request.POST)
+    if form.is_valid():
+        cd = form.cleaned_data
+        cart.add(product=product,
+                 quantity=cd['quantity'],
+                 update_quantity=cd['update'])
+    return redirect('cart_detail')
+
+def cart_remove(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    cart.remove(product)
+    return redirect('cart_detail')
+
+def cart_detail(request):
+    cart = Cart(request)
+    return render(request, 'cart/detail.html', {'cart': cart})
